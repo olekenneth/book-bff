557f0befb6a57ed72078c5993963db5a34e753df
Ole-Kenneth Bratholt <ok@vg.no>
2016-03-08 15:11:11 +0100

    Use jsonpath to make sure we always get the book object. Even if the format changes

--- routes/books.js
+++ routes/books.js
@@ -7,6 +7,7 @@ var goodGuy = require('good-guy-http');
 var request = goodGuy({
     maxRetries: 3
 });
+var jp = require('jsonpath');
 
 var proxies = [
     'https://book-catalog-proxy.herokuapp.com/book?isbn=',
@@ -25,26 +26,18 @@ var getBook = (isbn) => {
         url: proxies[Math.floor(Math.random() * proxies.length)] + isbn,
         json: true
     })
-        .then((response) => response.body)
-        .then((response) => {
-            console.log(response);
-            if (response && response.items && response.items.length > 0) {
-                return response.items;
-            }
-            throw new Error('No book found with that isbn: ' + isbn);
-        });
+        .then((response) => response.body);
 };
 
 router.get('/:isbn', function(req, res, next) {
     getBook(req.params.isbn)
-        .then((items) => {
-            var item = items[0];
+        .then((json) => {
             var book = {
-                title: item.volumeInfo.title,
-                subtitle: item.volumeInfo.subtitle,
-                image: item.volumeInfo.imageLinks.thumbnail
+                title: jp.query(json, '$..title'),
+                subtitle: jp.query(json, '$..subtitle'),
+                image: jp.query(json, '$..thumbnail')
             };
-            res.render('book', { title: 'Express', book: book });
+            res.render('book', { title: 'BFF for books', book: book });
         })
         .catch(next);
 });
